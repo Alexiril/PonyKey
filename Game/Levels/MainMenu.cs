e2a3ccdcc9cb50e7d476950a5e9f8b7cf3f21b3b
Alex.Kirel <alex.kirel.gm@gmail.com>
2023-04-08 16:50:49 +0500

    Removed redundant testing code.

--- Game/Levels/MainMenu.cs
+++ Game/Levels/MainMenu.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using Game.BaseTypes;
-using Game.BuiltInComponents;
+﻿using Game.BaseTypes;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace Game.Levels;
 
@@ -14,16 +11,11 @@ public abstract class MainMenu : ILevel
         {
             BackgroundColor = Color.White
         };
-        var button = result.AddGameObject(new GameObject("Button", actualGame));
-        button.AddComponent<Sprite>().Texture = actualGame.Content.Load<Texture2D>("SnakeTexture");
-        button.Transform.Position = new Vector2(520, 200);
-        button.Transform.Rotation = 20;
-        result.AddGameObject(new GameObject("Button1", actualGame)).AddComponent<Sprite>();
         return result;
     }
 
     public static void UnloadAssets(InternalGame actualGame)
     {
-        actualGame.Content.UnloadAssets(new List<string> {"SnakeTexture"});
+
     }
 }
